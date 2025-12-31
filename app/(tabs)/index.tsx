8a931228f278425a4151fb1b7dfaeb05e2942f1b
nvdeekay <khanhnvd.work@gmail.com>
2025-12-31 16:09:29 +0700

    routing and tab

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,8 +3,7 @@ import { Text, View } from 'react-native';
 export default function HomeScreen() {
   return (
     <View className='flex-1 justify-center items-center'>
-      <Text className='text-3xl font-bold text-dark-100'>Welcome nhaaaa!</Text>
+      <Text className='text-3xl font-bold text-dark-100'>Welcome!</Text>
     </View>
   );
 }
-
